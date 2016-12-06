318d9422aae573878f4bb3a260916dc179ece9af
Terry Tang <terrytangyuan@gmail.com>
2016-12-05 19:20:21 -0600

    Added test for iterator edge case

--- tests/testthat/test-python-iterators.R
+++ tests/testthat/test-python-iterators.R
@@ -6,11 +6,15 @@ test <- import("tftools.test")
 test_that("Iterators reflect values back", {
   rlist <- list("foo", "bar", 42L)
   expect_equal(iterate(test$makeIterator(rlist)), rlist)
+  a <- test$makeIterator(rlist)
+  b <- iterate(a)
+  expect_equal(b, rlist)
 })
 
 
 test_that("Generators reflect values back", {
   expect_equal(as.integer(iterate(test$makeGenerator(5))) + 1L, seq(5))
+  # Test iterate() returns
   a <- test$makeGenerator(5)
   b <- as.integer(iterate(a))
   expect_equal(b + 1L, seq(5))
