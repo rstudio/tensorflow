ddd148c1c568092d7d60a9ef36c4f038d3596978
JJ Allaire <jj@rstudio.org>
2017-02-08 18:00:02 -0500

    some additional reticlate re-exports

--- R/reexports.R
+++ R/reexports.R
@@ -15,3 +15,16 @@ reticulate::iterate
 #' @export
 reticulate::`%as%`
 
+#' @export
+reticulate::use_python
+
+#' @export
+reticulate::use_virtualenv
+
+#' @export
+reticulate::use_condaenv
+
+#' @export
+reticulate::py_config
+
+
