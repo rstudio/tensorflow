4e47f55491864a3cec90a19b30db499323ecfe1a
JJ Allaire <jj@rstudio.org>
2017-02-09 08:45:26 -0500

    remove unused test function

--- tests/testthat/utils.R
+++ tests/testthat/utils.R
@@ -1,8 +1,4 @@
 
-skip_if_no_python <- function() {
-  if (!reticulate::py_available())
-    skip("Python bindings not available for testing")
-}
 
 skip_if_no_tensorflow <- function() {
   have_tensorflow <- tryCatch({ reticulate::import("tensorflow"); TRUE }, error = function(e) FALSE)
