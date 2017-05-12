7c1cfe56c03d708b48ab36db6e60b1beb06cf2b1
JJ Allaire <jj@rstudio.com>
2017-05-12 10:08:51 -0400

    revise config error message

--- R/package.R
+++ R/package.R
@@ -75,12 +75,12 @@ tf_version <- function() {
 #' @keywords internal
 #' @export
 tf_config_error_message <- function() {
-  message <- "Unable to locate the 'tensorflow' Python package"
+  message <- "Installation of TensorFlow not found."
   config <- py_config()
   if (!is.null(config)) {
     if (length(config$python_versions) > 0) {
       message <- paste0(message,
-                        "\n\nPython environments searched for 'tensorflow':\n")
+                        "\n\nPython environments searched for 'tensorflow' package:\n")
       python_versions <- paste0(" ", config$python_versions, collapse = "\n")
       message <- paste0(message, python_versions, sep = "\n")
     }
