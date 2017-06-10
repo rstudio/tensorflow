919a450a0e92704693256dabee1162c3304f5e04
jjallaire <jj@rstudio.com>
2017-06-09 17:23:24 -0700

    improve logic for auto-binding; add action parameter

--- R/tensorboard.R
+++ R/tensorboard.R
@@ -1,37 +1,46 @@
 
 
 
-
-
 #' TensorBoard Visualization Tool
 #'
 #' TensorBoard is a tool inspecting and understanding your TensorFlow runs and
 #' graphs.
 #'
 #' @param log_dir Root directory for training logs.
+#' @param action Specify whether to start or stop TensorBoard for the
+#'   given `log_dir` (TensorBoard will be stopped automatically when
+#'   the R session from which it is launchedd is terminated).
 #' @param host Host for serving TensorBoard
 #' @param port Port for serving TensorBoard. If "auto" is specified (the
 #'   default) then an unused port will be chosen automatically.
 #' @param launch_browser `TRUE` to open a web browser for TensorBoard
 #'   after launching.
 #'
-#' @return URL for browsing TensorBoard (invisibly) or `NULL` if TensorBoard
-#'   could not be started.
+#' @return URL for browsing TensorBoard (invisibly).
 #'
 #' @details When TensorBoard is passed a logdir at startup, it recursively walks
 #'   the directory tree rooted at logdir looking for subdirectories that contain
 #'   tfevents data. Every time it encounters such a subdirectory, it loads it as
 #'   a new run, and the frontend will organize the data accordingly.
 #'
+#'   The TensorBoard process will be automatically destroyed when the R
+#'   session in which it is launched exits. You can pass `action = "stop"``
+#'   to manually terminate a TensorBoard process for a given `log_dir`.
+#'
 #' @seealso
 #' <https://github.com/tensorflow/tensorflow/blob/master/tensorflow/tensorboard/>
 #'
 #' @export
-tensorboard <- function(log_dir = ".", host = "127.0.0.1", port = "auto",
+tensorboard <- function(log_dir = ".", action = c("start", "stop"),
+                        host = "127.0.0.1", port = "auto",
                         launch_browser = interactive()) {
 
   # ensure that tensorflow initializes (so we get tensorboard on our path)
   ensure_loaded()
+
+  # verify we can find tensorboard
+  if (!nzchar(Sys.which("tensorboard")))
+    stop("Unable to find tensorboard on PATH")
 
   # expand log dir
   log_dir <- normalizePath(log_dir, mustWork = FALSE)
@@ -47,21 +56,65 @@ tensorboard <- function(log_dir = ".", host = "127.0.0.1", port = "auto",
     if (p$is_alive()) {
       p$kill()
       p$wait(1000L)
-      if (identical(port, "auto"))
-        port <- tb$port
     }
+    if (identical(port, "auto"))
+      port <- tb$port
   }
 
-  # determine port
-  explicit_port <- !identical(port, "auto")
-  if (!explicit_port) {
-    # generate a port (but don't use ports that Chrome considers unsafe)
-    while(TRUE) {
-      port <- 3000 + sample(5000, 1)
-      if (!port %in% c(3659, 4045, 6000, 6665:6669))
+  # exit if this was action = "stop"
+  action <- match.arg(action)
+  if (identical(action, "stop")) {
+    cat("TensorBoard stopped.\n")
+    return(invisible(NULL))
+  }
+
+
+  # for port = "auto", attempt to find a port up to 20 times
+  if (identical(port, "auto")) {
+
+    for (i in 1:20) {
+
+      # determine the port (exclude those considered unsafe by Chrome)
+      while(TRUE) {
+        port <- 3000 + sample(5000, 1)
+        if (!port %in% c(3659, 4045, 6000, 6665:6669))
+          break
+      }
+
+      # attempt to launch
+      p <- launch_tensorboard(log_dir, host, port, FALSE)
+      if (p$is_alive())
         break
     }
+
+  } else {
+    p <- launch_tensorboard(log_dir, host, port, TRUE)
   }
+
+  if (p$is_alive()) {
+
+    # close connections
+    close(p$get_output_connection())
+    close(p$get_error_connection())
+
+    # add to global list of tensorboards
+    .globals$tensorboards[[log_dir]] <- list(process = p, port = port)
+
+    # browse the url if requested
+    url <- paste0("http://", host, ":", port)
+    cat("Started TensorBoard at", url, "\n")
+    if (launch_browser)
+      utils::browseURL(url)
+
+    # return the url invisibly
+    invisible(url)
+
+  } else {
+    stop("Unable to launch tensorboard")
+  }
+}
+
+launch_tensorboard <- function(log_dir, host, port, explicit_port) {
 
   # start the process
   p <- processx::process$new("tensorboard",
@@ -70,9 +123,9 @@ tensorboard <- function(log_dir = ".", host = "127.0.0.1", port = "auto",
                                "--port", as.character(port)),
                              stdout = "|", stderr = "|")
 
-  # poll for output until we get the port
+  # poll for output until we've succesfully started up or the process dies
   started <- FALSE
-  while(!started) {
+  while(!started && p$is_alive()) {
 
     # poll for io
     res <- p$poll_io(100L)
@@ -83,13 +136,12 @@ tensorboard <- function(log_dir = ".", host = "127.0.0.1", port = "auto",
       # capture output
       out <- p$read_output_lines()
 
-      # see if we have started
-      if (any(grepl("http://", out, fixed = TRUE))) {
-        write(paste0("Started TensorBoard at http://", host,":", port, "\n"), stdout())
+      # check if it's a startup notification. if it is then set the started flag,
+      # otherwise just forward the ouptut
+      if (any(grepl("http://", out, fixed = TRUE)))
         started <- TRUE
-      } else {
+      else
         write(out, stdout())
-      }
     }
 
     # see if we have stderr
@@ -98,38 +150,14 @@ tensorboard <- function(log_dir = ".", host = "127.0.0.1", port = "auto",
       # capture error output
       err <- p$read_error_lines()
 
-      # if it's 'already in use' and there is no explicit port then call the function back
-      # to try another random port
-      if (!explicit_port && any(grepl(paste0("^.*", port, ".*already in use.*$"), err))) {
-        return (tensorboard(log_dir = log_dir, host = host, port = "auto"))
-      } else {
+      # write it unless it's a port in use error when we are auto-binding
+      if (explicit_port || !any(grepl(paste0("^.*", port, ".*already in use.*$"), err)))
         write(err, stderr())
-      }
     }
-
-    # abort if the process is dead
-    if (!p$is_alive())
-      break
   }
 
-  # success!
-  if (p$is_alive()) {
-
-    # close connections
-    close(p$get_output_connection())
-    close(p$get_error_connection())
-
-    # add to global list of tensorboards
-    .globals$tensorboards[[log_dir]] <- list(process = p, port = port)
-
-    # browse the url
-    url <- paste0("http://", host,":", port)
-    if (launch_browser)
-      utils::browseURL(url)
-
-    # return the url invisibly
-    invisible(url)
-  } else {
-    invisible(NULL)
-  }
+  # return the process
+  p
 }
+
+
