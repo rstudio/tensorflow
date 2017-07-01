2651e8744b000c687c89525956230e1993b44220
jjallaire <jj@rstudio.com>
2017-07-01 11:20:33 -0400

    always run all tests on travis; use non-container travis image so we can get Ubuntu 14.04

--- tests/testthat/utils.R
+++ tests/testthat/utils.R
@@ -1,6 +1,10 @@
 
 
+is_travis <- function() {
+  identical(Sys.getenv("TRAVIS"), "true")
+}
+
 skip_if_no_tensorflow <- function() {
-  if (!reticulate::py_module_available("tensorflow"))
+  if (!is_travis() && !reticulate::py_module_available("tensorflow"))
     skip("TensorFlow not available for testing")
 }
