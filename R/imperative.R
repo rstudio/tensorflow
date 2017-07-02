6d239f80dfd81058eb8d38658ef8b165cdf9d3b1
terrytangyuan <terrytangyuan@gmail.com>
2017-07-02 16:20:07 -0400

    Allow to convert all Tensors to R objects

--- R/imperative.R
+++ R/imperative.R
@@ -8,6 +8,9 @@
 #' the standard TensorFlow runtime, and also for allowing automatic differentiation via `tf$gradients`.
 #' If this is set to `TRUE`, the graph as well as the cached tensors that have been kept around for
 #' gradient computation will be cleared after the expression is evaluated.
+#' @param convert A boolean indicating whether to convert the Tensor objects inside the context
+#' scope to R objects.
+#' @param env A specific environment to convert the Tensor objects to.
 #' 
 #' @examples 
 #' \dontrun{
@@ -20,16 +23,34 @@
 #' 
 #' }
 #' @export
-with_imperative <- function(expr, new_step = FALSE) {
+with_imperative <- function(
+  expr,
+  new_step = FALSE,
+  convert = TRUE,
+  env = parent.env(environment())
+) {
   server <- tf$python$training$training$Server$create_local_server()
   target <- server$target
   with(tf$contrib$imperative$imperative_mode$ImperativeMode(target), as = imperative_mode, {
     if (new_step) {
       with(imperative_mode$new_step(), as = new_step, {
-        expr
+        invisible(expr)
+        if (convert) invisible(implicit_eval_tensor(env = env))
       })
     } else {
-      expr
+      invisible(expr)
+      if (convert) invisible(implicit_eval_tensor(env = env))
+    }
+  })
+}
+
+# convert all Tensor objects to R objects in an environment
+implicit_eval_tensor <- function(env) {
+  var_names <- ls(envir = env)
+  lapply(var_names, function(var_name) {
+    var <- get(var_name, envir = env)
+    if (inherits(var, "tensorflow.python.framework.ops.Tensor")) {
+      assign(var_name, var$eval(), envir = env)
     }
   })
 }
--- tests/testthat/test-imperative.R
+++ tests/testthat/test-imperative.R
@@ -6,22 +6,37 @@ test_that("Imperative style TensorFlow works", {
   list_b <- list(list(6, 7))
   list_c <- list(list(28), list(24))
   list_d <- list(list(42, 49), list(36, 42))
+  
+  # convert == FALSE
+  for(new_step in c(TRUE, FALSE)) {
+    with_imperative({
+      a_const <- tf$constant(list_a)
+      b_const <- tf$constant(list_b)
+      c_const <- a_const * 4
+      d_const <- tf$matmul(a_const, b_const)
+      expect_equal(c_const, tf$constant(list_c))
+      expect_equal(d_const, tf$constant(list_d))
+    }, new_step = new_step, convert = FALSE, env = environment())
+    expect_false(inherits(a_const, "matrix"))
+    expect_false(inherits(b_const, "matrix"))
+    expect_false(inherits(c_const, "matrix"))
+    expect_false(inherits(d_const, "matrix"))
+  }
 
-  with_imperative({
-    a <- tf$constant(list_a)
-    b <- tf$constant(list_b)
-    c <- a * 4
-    d <- tf$matmul(a, b)
-    expect_equal(c, tf$constant(list_c))
-    expect_equal(d, tf$constant(list_d))
-  }, TRUE)
-
-  with_imperative({
-    a <- tf$constant(list_a)
-    b <- tf$constant(list_b)
-    c <- a * 4
-    d <- tf$matmul(a, b)
-    expect_equal(c, tf$constant(list_c))
-    expect_equal(d, tf$constant(list_d))
-  }, FALSE)
+  # convert == TRUE
+  for(new_step in c(TRUE, FALSE)) {
+    with_imperative({
+      a_const <- tf$constant(list_a)
+      b_const <- tf$constant(list_b)
+      c_const <- a_const * 4
+      d_const <- tf$matmul(a_const, b_const)
+      expect_equal(c_const, tf$constant(list_c))
+      expect_equal(d_const, tf$constant(list_d))
+    }, new_step = new_step, convert = TRUE, env = environment())
+    expect_equal(a_const, matrix(unlist(list_a), nrow = 2))
+    expect_equal(b_const, matrix(unlist(list_b), nrow = 1))
+    expect_equal(c_const, matrix(unlist(list_c), nrow = 2))
+    expect_equal(d_const, matrix(unlist(list_d), nrow = 2, byrow = TRUE))
+  }
+  
 })
