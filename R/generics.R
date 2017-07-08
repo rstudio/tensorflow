a14adfce3912f11e5500d7e5b48059cf18c2830a
terrytangyuan <terrytangyuan@gmail.com>
2017-07-08 18:08:32 -0400

    Add functions for dimensionality retrieval for tensors

--- R/generics.R
+++ R/generics.R
@@ -37,6 +37,26 @@ print.tensorflow.python.ops.variables.Variable <- print.tensorflow.python.framew
   names(flags)
 }
 
+#' @export
+dim.tensorflow.python.framework.ops.Tensor <- function(x) {
+  x$get_shape()$as_list()
+}
+
+#' @export
+length.tensorflow.python.framework.ops.Tensor <- function(x) {
+  Reduce(`*`, dim(x))
+}
+
+#' @export
+nrow.tensorflow.python.framework.ops.Tensor <- function(x) {
+  dim(x)[[1]]
+}
+
+#' @export
+ncol.tensorflow.python.framework.ops.Tensor <- function(x) {
+  dim(x)[[2]]
+}
+
 # https://stat.ethz.ch/R-manual/R-devel/library/base/html/InternalMethods.html
 
 
