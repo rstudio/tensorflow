5e59f3793cb1a301ab8bdfc2ed7e3febe050a717
jjallaire <jj@rstudio.com>
2017-07-16 14:51:16 -0400

    latest_run and list_runs functions

--- R/runs.R
+++ R/runs.R
@@ -1,32 +1,5 @@
 
-#' Create a timestamp based unique directory name
-#'
-#' Directory name is a timestamp compatible with filesystem naming restrictions
-#' (e.g. "2017-07-13T16-18-51.569Z").
-#'
-#' @param parent_dir Parent directory
-#' @param prefix Character value to prepend to directory name
-#'
-#' @return Unique directory name
-#'
-#' @note The name of the directory is based on the current time (in milliseconds
-#'   resolution). If the generated directory name already exists then the
-#'   function will wait long enough to return a directory with a distinct
-#'   timestamp.
-#'
-#' @seealso [run_dir()]
-#'
-#' @export
-unique_dir <- function(parent_dir, prefix = NULL, format = "%Y-%m-%dT%H-%M-%OS3Z") {
-  while(TRUE) {
-    dir <- file.path(parent_dir,
-                     paste0(prefix, strftime(Sys.time(), format = format, tz = "gmt")))
-    if (!file.exists(dir))
-      return(dir)
-    else
-      Sys.sleep(0.1)
-  }
-}
+
 
 #' Run Directory
 #'
@@ -34,17 +7,68 @@ unique_dir <- function(parent_dir, prefix = NULL, format = "%Y-%m-%dT%H-%M-%OS3Z
 #' location for each training run. Note that if the `RUNDIR` environment
 #' variable is availale it will be returned as the run directory.
 #'
-#' @param parent_dir Parent to create run directory within.
+#' @param runs_dir Parent directory for runs
+#' @param latest_n List only the latest n runs
 #'
-#' @return Timestamped run directory.
+#' @return `run_dir()` retruns a timestamped run directory. The
+#'  `list_runs()` function returns a character vector with the
+#'  paths of existing run directories (`latest_run()` is a convenience
+#'  function for getting the path to the most recent run directory).
 #'
 #' @export
-run_dir <- function(parent_dir = "runs") {
+run_dir <- function(runs_dir = "runs") {
   dir <- Sys.getenv("RUNDIR", unset = NA)
   if (is.na(dir))
-    dir <- unique_dir(parent_dir)
+    dir <- unique_dir(runs_dir, format = "%Y-%m-%dT%H-%M-%SZ")
   if (!utils::file_test("-d", dir))
     dir.create(dir, recursive = TRUE)
   dir
 }
+
+#' @rdname run_dir
+#' @export
+latest_run <- function(runs_dir = "runs") {
+  list_runs(runs_dir, latest_n = 1)
+}
+
+
+#' @rdname run_dir
+#' @export
+list_runs <- function(runs_dir = "runs", latest_n = NULL) {
+  if (file.exists(runs_dir)) {
+    runs <- list.files(runs_dir,
+                       pattern = "\\d{4}-\\d{2}-\\d{2}T\\d{2}-\\d{2}-\\d{2}Z",
+                       full.names = FALSE)
+    if (length(runs) > 0) {
+      runs <- runs[order(runs, decreasing = TRUE)]
+      if (!is.null(latest_n))
+        runs <- runs[1:min(length(runs),latest_n)]
+      runs <- data.frame(created = as.POSIXct(runs, format = "%Y-%m-%dT%H-%M-%SZ"),
+                         run_dir = file.path(runs_dir, runs),
+                         stringsAsFactors = FALSE)
+      runs <- runs[order(runs$created , decreasing = TRUE ),]
+      runs$run_dir
+    } else {
+      character()
+    }
+  }
+  else
+    character()
+}
+
+
+unique_dir <- function(parent_dir, prefix = NULL, format = "%Y-%m-%dT%H-%M-%SZ") {
+  while(TRUE) {
+    dir <- file.path(parent_dir,
+                     paste0(prefix, strftime(Sys.time(), format = format, tz = "GMT")))
+    if (!file.exists(dir))
+      return(dir)
+    else
+      Sys.sleep(0.1)
+  }
+}
+
+
+
+
 
