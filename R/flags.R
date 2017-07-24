3c8d203ea5c6bd2743fc9f85b9eca9d99617cb72
jjallaire <jj@rstudio.com>
2017-07-24 15:53:37 -0400

    dontrun flags example (fails when no tf)

--- R/flags.R
+++ R/flags.R
@@ -14,9 +14,36 @@
 #'   character vector) to be parsed.
 #'
 #' @return A named \R list, mapping configuration keys to values.
+
+#' @examples
+#' \dontrun{
+#' # examine an example configuration file provided by tensorflow
+#' file <- system.file("examples/config/flags.yml", package = "tensorflow")
+#' cat(readLines(file), sep = "\n")
 #'
+#' # read the default configuration
+#' FLAGS <- tensorflow::parse_flags("default", file = file)
+#' str(FLAGS)
+#'
+#' # read the alternate configuration: note that
+#' # the default configuration is inherited, but
+#' # we override the 'string' configuration here
+#' FLAGS <- tensorflow::parse_flags("alternate", file = file)
+#' str(FLAGS)
+#'
+#' # override configuration values using command
+#' # line arguments (normally, these would be
+#' # passed in through the command line invocation
+#' # used to start the process)
+#' FLAGS <- tensorflow::parse_flags(
+#'   "alternate",
+#'   file = file,
+#'   arguments = c("--foo=1")
+#' )
+#' str(FLAGS)
+#'
+#' }
 #' @export
-#' @example examples/examples-flags.R
 parse_flags <-
   function(config = Sys.getenv("R_CONFIG_ACTIVE", unset = "default"),
            file = "flags.yml",
