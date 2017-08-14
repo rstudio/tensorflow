3229331c315b13657d60a314cace8c3cb4a8b61f
jjallaire <jj@rstudio.com>
2017-08-14 09:24:29 -0400

    re-export flags and run_dir from tfruns

--- R/reexports.R
+++ R/reexports.R
@@ -24,4 +24,29 @@ reticulate::use_virtualenv
 #' @export
 reticulate::use_condaenv
 
+#' @importFrom tfruns flags
+#' @export
+tfruns::flags
+
+#' @importFrom tfruns flag_numeric
+#' @export
+tfruns::flag_numeric
+
+#' @importFrom tfruns flag_integer
+#' @export
+tfruns::flag_integer
+
+#' @importFrom tfruns flag_string
+#' @export
+tfruns::flag_string
+
+#' @importFrom tfruns flag_boolean
+#' @export
+tfruns::flag_boolean
+
+#' @importFrom tfruns run_dir
+#' @export
+tfruns::run_dir
+
+
 
