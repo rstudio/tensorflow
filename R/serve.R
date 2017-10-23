df7201688ca6d9e78b874cae7edadcfdb4b97013
Javier Luraschi <javierluraschi@hotmail.com>
2017-10-23 15:27:33 -0700

    add serve daemonized option to allow for test

--- R/serve.R
+++ R/serve.R
@@ -8,12 +8,14 @@ serve <- function(
   model_dir,
   host = "127.0.0.1",
   port = 8089,
-  browse = interactive()
+  browse = interactive(),
+  daemonized = FALSE
   ) {
-  server_run(model_dir, host, port, httpuv::runServer, browse)
+  httpuv_start <- if (daemonized) httpuv::startDaemonizedServer else httpuv::runServer
+  serve_run(model_dir, host, port, httpuv_start, browse)
 }
 
-server_load_model <- function(sess, model_dir) {
+serve_load_model <- function(sess, model_dir) {
   tf$reset_default_graph()
 
   graph <- tf$saved_model$loader$load(
@@ -24,7 +26,7 @@ server_load_model <- function(sess, model_dir) {
   graph$signature_def
 }
 
-server_content_type <- function(file_path) {
+serve_content_type <- function(file_path) {
   file_split <- strsplit(file_path, split = "\\.")[[1]]
   switch(file_split[[length(file_split)]],
          "css" = "text/css",
@@ -36,20 +38,20 @@ server_content_type <- function(file_path) {
   )
 }
 
-server_static_file_response <- function(file_path) {
+serve_static_file_response <- function(file_path) {
   file_path <- system.file(file_path, package = "tfserve")
   file_contents <- if (file.exists(file_path)) readBin(file_path, "raw", n = file.info(file_path)$size) else NULL
 
   list(
     status = 200L,
     headers = list(
-      "Content-Type" = paste0(server_content_type(file_path))
+      "Content-Type" = paste0(serve_content_type(file_path))
     ),
     body = file_contents
   )
 }
 
-server_invalid_request <- function(message = NULL) {
+serve_invalid_request <- function(message = NULL) {
   list(
     status = 404L,
     headers = list(
@@ -64,13 +66,13 @@ server_invalid_request <- function(message = NULL) {
   )
 }
 
-server_handlers <- function(host, port) {
+serve_handlers <- function(host, port) {
   list(
     "^/swagger.json" = function(req, sess, signature_def) {
       list(
         status = 200L,
         headers = list(
-          "Content-Type" = paste0(server_content_type("json"), "; charset=UTF-8")
+          "Content-Type" = paste0(serve_content_type("json"), "; charset=UTF-8")
         ),
         body = charToRaw(enc2utf8(
           swagger_from_signature_def(signature_def, host, port)
@@ -78,17 +80,17 @@ server_handlers <- function(host, port) {
       )
     },
     "^/$" = function(req, sess, signature_def) {
-      server_static_file_response("swagger-ui/index.html")
+      serve_static_file_response("swagger-ui/index.html")
     },
     "^/[^/]*$" = function(req, sess, signature_def) {
-      server_static_file_response(file.path("swagger-ui", req$PATH_INFO))
+      serve_static_file_response(file.path("swagger-ui", req$PATH_INFO))
     },
     "^/api/[^/]*/predict" = function(req, sess, signature_def) {
       signature_names <- signature_def$keys()
       signature_name <- strsplit(req$PATH_INFO, "/")[[1]][[3]]
 
       if (!signature_name %in% signature_names) {
-        server_invalid_request()
+        serve_invalid_request()
         return()
       }
 
@@ -97,7 +99,7 @@ server_handlers <- function(host, port) {
 
       tensor_input_names <- signature_def$get(signature_name)$inputs$keys()
       if (length(tensor_input_names) != 1) {
-        server_invalid_request("Currently, only single-tensor inputs are supported but found ", length(tensor_input_names))
+        serve_invalid_request("Currently, only single-tensor inputs are supported but found ", length(tensor_input_names))
         return()
       }
 
@@ -119,7 +121,7 @@ server_handlers <- function(host, port) {
       list(
         status = 200L,
         headers = list(
-          "Content-Type" = paste0(server_content_type("json"), "; charset=UTF-8")
+          "Content-Type" = paste0(serve_content_type("json"), "; charset=UTF-8")
         ),
         body = charToRaw(enc2utf8(
           jsonlite::toJSON(result)
@@ -127,18 +129,18 @@ server_handlers <- function(host, port) {
       )
     },
     ".*" = function(req, signature_def) {
-      server_invalid_request()
+      serve_invalid_request()
     }
   )
 }
 
-server_run <- function(model_dir, host, port, start, browse) {
+serve_run <- function(model_dir, host, port, start, browse) {
   sess <- tf$Session()
-  signature_def <- server_load_model(sess, model_dir)
+  signature_def <- serve_load_model(sess, model_dir)
 
   if (browse) utils::browseURL(paste0("http://", host, ":", port))
 
-  handlers <- server_handlers(host, port)
+  handlers <- serve_handlers(host, port)
 
   start(host, port, list(
     onHeaders = function(req) {
