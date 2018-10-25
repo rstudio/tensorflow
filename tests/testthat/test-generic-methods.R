5b3cf6033125d4bc574f3fb4f2513c65a1d79ff4
Tomasz Kalinowski <kalinowskit@gmail.com>
2018-10-24 21:19:10 -0400

    support log() with arbritray base, log2(), log10()

---
+++ tests/testthat/test-generic-methods.R
@@ -0,0 +1,34 @@
+context("generic methods")
+
+grab <- (function() {
+  sess <- tf$Session()
+  function(x)
+    sess$run(x)
+})()
+
+as_tensor <- tf$convert_to_tensor
+
+expect_near <- function(..., tol = 1e-5) expect_equal(..., tolerance = tol)
+
+test_that("log with supplied base works", {
+
+  r <- array(as.double(1:20))
+  t <- as_tensor(r, dtype = tf$float32)
+
+  expect_near(r, grab(   log( as_tensor(exp(r)))))
+  expect_near(r, grab(log2(  as_tensor(2 ^ r)) ))
+  expect_near(r, grab(log10( as_tensor(10 ^ r)) ))
+
+  expect_near(r, grab(   log( exp(t))))
+  expect_near(r, grab(  log2(  2 ^ t )))
+  expect_near(r, grab( log10( 10 ^ t )))
+
+  # log() dispatches correctly without trying to change base
+  expect_identical(grab(tf$log(t)), grab(log(t)))
+
+  expect_near(log(r), grab(log(t)))
+  expect_near(log(r, base = 3), grab(log(t, base = 3)))
+
+})
+
+
