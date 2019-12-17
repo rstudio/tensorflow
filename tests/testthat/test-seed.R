63c95b4a76af02e9ca51bce99bb44a59923fb303
Daniel Falbel <dfalbel@gmail.com>
2019-12-17 10:47:48 -0300

    use named argument

--- tests/testthat/test-seed.R
+++ tests/testthat/test-seed.R
@@ -3,7 +3,7 @@ test_that("use_session_with_seed works", {
 
   f <- function() {
     library(keras)
-    use_session_with_seed(1)
+    use_session_with_seed(seed = 1)
     model <- keras_model_sequential() %>%
       layer_dense(units = 1)
     predict(model, matrix(1, ncol = 1))
