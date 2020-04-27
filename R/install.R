bcb65a26af64b1510dc166a7116fb27888acc2de
skeydan <keydana@gmx.de>
2020-04-27 09:20:13 +0200

    default TF version is 2.1

--- R/install.R
+++ R/install.R
@@ -84,7 +84,7 @@ install_tensorflow <- function(method = c("auto", "virtualenv", "conda"),
 
 parse_tensorflow_version <- function(version) {
 
-  default_version <- "2.0.0"
+  default_version <- "2.1.0"
 
   ver <- list(
     version = default_version,
