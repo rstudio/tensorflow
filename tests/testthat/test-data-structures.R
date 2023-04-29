758282c90d97b8f58b9e2811cefd6d64a706b4c1
Tomasz Kalinowski <kalinowskit@gmail.com>
2023-04-29 08:59:54 -0400

    expand tests for tf data_structures
    Fixed in https://github.com/rstudio/reticulate/pull/1366

--- tests/testthat/test-data-structures.R
+++ tests/testthat/test-data-structures.R
@@ -1,6 +1,15 @@
 test_that("cast List wrapers", {
   model <- tf$keras$models$Sequential(list())
-  model$denses <- list(tf$keras$layers$Dense(10L), tf$keras$layers$Dense(10L))
+  model$denses <- list(tf$keras$layers$Dense(10L),
+                       tf$keras$layers$Dense(10L))
+
+  model$denses_dict <- list(abc = tf$keras$layers$Dense(10L),
+                            def = tf$keras$layers$Dense(10L))
 
   expect_true(is.list(model$denses))
+  expect_true(length(model$denses) == 2)
+
+  expect_true(is.list(model$denses_dict))
+  expect_true(length(model$denses_dict) == 2)
+  expect_named(model$denses_dict, c("abc", "def"))
 })
