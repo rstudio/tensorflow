3a70b247f50dfda2664cc6db8fcdff2121da2cd9
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-02-24 17:06:27 -0500

    add (failing) test

--- tests/testthat/test-types.R
+++ tests/testthat/test-types.R
@@ -12,3 +12,18 @@ test_that("TensorShapes are not converted to lists", {
   expect_true(y[2] == shape(10))
   expect_identical(y[[2]], 10L)
 })
+
+
+test_that("tf.random works", {
+  # Installing tensorflow-metal 1.2.0 makes this error.
+  x <- tf$random$stateless_uniform(
+    shape = tuple(10L),
+    seed = tuple(2L, 3L),
+    minval = 0L,
+    maxval = 10L,
+    dtype = tf$dtypes$int32)
+  expect_s3_class(x, "tensorflow.tensor")
+  x <- as.array(x)
+  expect_type(x, "integer")
+  expect_identical(dim(x), 10L)
+})
